325f5c7e7cfcf51a0ff67beb4827f41fde6fbd9c
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-15 22:26:17 +0300

    Initial Commit

---
+++ MiCard SwiftUI/MiCard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiCard_SwiftUIApp.swift
+//  MiCard SwiftUI
+//
+//  Created by ALEKSEY SUSLOV on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MiCard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
